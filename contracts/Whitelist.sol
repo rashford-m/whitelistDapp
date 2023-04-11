efa553d3f5828f6e5d967719cff13fe71b0e88b7
Rashford <mikearowo@gmail.com>
2023-04-11 20:56:57 +0100

    whitelist dapp

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.0;
+
+
+contract Whitelist {
+    // Max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
+}
