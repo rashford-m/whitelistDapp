7e8f0fe3ea5d46d98e5fc3750d522c35fdf9800a
Rashford <mikearowo@gmail.com>
2023-04-27 11:09:32 +0100

    updated

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -5,4 +5,27 @@ pragma solidity 0.8.0;
 contract Whitelist {
     // Max number of whitelisted addresses allowed
     uint8 public maxWhitelistedAddresses;
+    // Create a mapping of whitelistedAddresses
+    // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
+    mapping(addess => bool) public whitelistedAddresses;
+
+    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
+    // NOTE: Don't change this variable name, as it will be part of verification
+    uint8 public numAddressesWhitelisted;
+
+// Setting the Max number of whitelisted addresses
+    // User will put the value at the time of deployment
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    /**
+        addAddressToWhitelist - This function adds the address of the sender to the
+        whitelist
+     */
+
+    function addAddressToWhitelist() public {
+        // check if the user has already been whitelisted
+    }
+
 }
