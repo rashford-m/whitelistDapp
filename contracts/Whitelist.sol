3f1a9e6191208bca78c95900944432bc2a3ae1f4
Rashford <mikearowo@gmail.com>
2023-04-28 10:21:40 +0100

    updated

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.0;
+pragma solidity 0.8.19;
 
 
 contract Whitelist {
@@ -7,7 +7,7 @@ contract Whitelist {
     uint8 public maxWhitelistedAddresses;
     // Create a mapping of whitelistedAddresses
     // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
-    mapping(addess => bool) public whitelistedAddresses;
+    mapping(address => bool) public whitelistedAddresses;
 
     // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
     // NOTE: Don't change this variable name, as it will be part of verification
@@ -26,6 +26,17 @@ contract Whitelist {
 
     function addAddressToWhitelist() public {
         // check if the user has already been whitelisted
+        require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+
+         // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+         require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
+
+// Add the address which called the function to the whitelistedAddress array
+whitelistedAddresses[msg.sender] = true;
+
+// Increase the number of whitelisted addresses
+numAddressesWhitelisted += 1;
+
     }
 
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,26 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-
+const { ethers } = require("hardhat");
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so whitelistContract here is a factory for instances of our Whitelist contract.
+  */
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  // Here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // 10 is the maximum number of whitelisted addresses allowed
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // wait for it to finish deploying
+  await deployedWhitelistContract.deployed();
 
-  await lock.deployed();
+  // print the address of the deployed contract
+  console.log("Whitelist contract address:", deployedWhitelistContract.address);
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  // call the main function and catch if there is any error
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+};
